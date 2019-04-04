275f29423dbad568c8343bf9e20fb5c3769abb13
akmalist <akmalqa101@gmail.com>
2019-04-03 21:07:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,19 @@ function assignGrade(score) {
 for (var i = 80; i <= 100; i++) {
   console.log('For ' + i + ', you got a ' + assignGrade(i));
 }
+
+
+// Write a for loop that will iterate from 0 to 20. For each iteration,
+//     it will check if the current number is even or odd, 
+//     and report that to the screen (e.g. "2 is even").
+
+for (let i=0; i<=20; i++){
+  if (i%2===0){
+    console.log(i+' is even')
+  } else {
+    console.log(i+ ' is odd')
+  }
+};
+
+
+
